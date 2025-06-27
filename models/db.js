041449e53797a5e164fd714b5a745c6127020035
Fogel <fogel98nex@gmail.com>
2025-06-27 00:50:11 -0600

    arreglo model

--- models/db.js
+++ models/db.js
@@ -2,9 +2,10 @@ const mysql = require("mysql2");
 
 const connection = mysql.createPool({
   host: "brj1xh515mz5mcb7eeqf-mysql.services.clever-cloud.com",
+  port: 3306, // o el que te indique Clever Cloud
   user: "uvtjqdtrnbzhubov",
   password: "mnM0bVsyqR4MiIRChXW3",
-  database: "brj1xh515mz5mcb7eeqf",
+  database: "brj1xh515mz5mcb7ee6",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
